2841d3146e5c17065751e47faa0fb15aa254b397
Luciano Nicacio <lucianonicacio.web@gmail.com>
2021-01-25 05:26:36 -0500

    finished pages header

---
+++ app/public/wp-content/themes/understrap-master/src/js/custom-javascript.js
@@ -0,0 +1,20 @@
+// Add your JS customizations here
+const sub = document.querySelector(".subtitle");
+const toggle = document.querySelector(".navbar-toggler");
+
+
+toggle.addEventListener('click', () =>{
+    sub.classList.add('display-none');
+});
+
+
+// toggle.addEventListener('click', () =>{
+//     sub.classList.remove('display-none');
+// });
+
+
+
+
+
+
+
